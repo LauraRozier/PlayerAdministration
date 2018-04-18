c9e4c5f854a0849fffb4eab14fd83999cabc0f19
Thimo Braker <thibmorozier@live.nl>
2018-04-18 22:07:07 +0200

    Name should be null, not empty str

--- PlayerAdministration.cs
+++ PlayerAdministration.cs
@@ -214,7 +214,7 @@ namespace Oxide.Plugins
                                    CuiRect anchor,
                                    bool cursorEnabled,
                                    CuiColor color = null,
-                                   string name = "",
+                                   string name = null,
                                    string png = null) => AddPanel(parent, anchor, new CuiRect(), cursorEnabled, color, name, png);
 
             /// <summary>
@@ -233,7 +233,7 @@ namespace Oxide.Plugins
                                    CuiRect offset,
                                    bool cursorEnabled,
                                    CuiColor color = null,
-                                   string name = "",
+                                   string name = null,
                                    string png = null)
             {
                 CuiPanel panel = new CuiPanel() {
@@ -254,7 +254,7 @@ namespace Oxide.Plugins
                     };
                 }
 
-                return container.Add(panel, parent, name ?? "");
+                return container.Add(panel, parent, name);
             }
 
             /// <summary>
@@ -272,7 +272,7 @@ namespace Oxide.Plugins
                                    CuiRect anchor,
                                    CuiColor color,
                                    string text,
-                                   string name = "",
+                                   string name = null,
                                    int fontSize = 14,
                                    TextAnchor align = TextAnchor.UpperLeft) => AddLabel(parent, anchor, new CuiRect(), color, text, name, fontSize, align);
 
@@ -293,7 +293,7 @@ namespace Oxide.Plugins
                                    CuiRect offset,
                                    CuiColor color,
                                    string text,
-                                   string name = "",
+                                   string name = null,
                                    int fontSize = 14,
                                    TextAnchor align = TextAnchor.UpperLeft)
             {
@@ -310,7 +310,7 @@ namespace Oxide.Plugins
                         OffsetMin = offset.GetPosMin(),
                         OffsetMax = offset.GetPosMax()
                     }
-                }, parent, name ?? "");
+                }, parent, name);
             }
 
             /// <summary>
@@ -334,7 +334,7 @@ namespace Oxide.Plugins
                                     string text,
                                     string command = "",
                                     string close = "",
-                                    string name = "",
+                                    string name = null,
                                     int fontSize = 14,
                                     TextAnchor align = TextAnchor.MiddleCenter) => AddButton(parent, anchor, new CuiRect(), buttonColor, textColor, text, command, close, name, fontSize, align);
 
@@ -361,7 +361,7 @@ namespace Oxide.Plugins
                                     string text,
                                     string command = "",
                                     string close = "",
-                                    string name = "",
+                                    string name = null,
                                     int fontSize = 14,
                                     TextAnchor align = TextAnchor.MiddleCenter)
             {
@@ -383,7 +383,7 @@ namespace Oxide.Plugins
                         Align = align,
                         Color = textColor.ToString()
                     }
-                }, parent, name ?? "");
+                }, parent, name);
             }
 
             /// <summary>
@@ -407,7 +407,7 @@ namespace Oxide.Plugins
                                         int charsLimit = 100,
                                         string command = "",
                                         bool isPassword = false,
-                                        string name = "",
+                                        string name = null,
                                         int fontSize = 14,
                                         TextAnchor align = TextAnchor.MiddleLeft) => AddInputField(parent, anchor, new CuiRect(), color, text, charsLimit, command, isPassword, name, fontSize, align);
 
@@ -434,7 +434,7 @@ namespace Oxide.Plugins
                                         int charsLimit = 100,
                                         string command = "",
                                         bool isPassword = false,
-                                        string name = "",
+                                        string name = null,
                                         int fontSize = 14,
                                         TextAnchor align = TextAnchor.MiddleLeft)
             {
@@ -454,7 +454,7 @@ namespace Oxide.Plugins
                         OffsetMin = offset.GetPosMin(),
                         OffsetMax = offset.GetPosMax()
                     }
-                }, parent, name ?? "");
+                }, parent, name);
             }
 
             /// <summary>
